b00c7bfa64d930cba67158cdb37f2eef3778bc9f
Okazaki Kohei <marie673g@gmail.com>
2021-07-09 21:31:15 +0900

    Vagrantfile編集

---
+++ vagrant_setup.sh
@@ -0,0 +1,15 @@
+echo set up docker
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt autoremove -y
+sudo apt install -y apache2
+
+sudo apt install -y apt-transport-https ca-certificates software-properties-common curl
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt fingerprint 0EBFCD88
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+
+sudo apt update -y
+sudo apt install -y docker-ce
+
+sudo usermod -aG docker docker
